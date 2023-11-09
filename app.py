58be4c1d8346fee4a720504606d1216e8a420fcd
Sung Hoon KIM <92258672+newcave@users.noreply.github.com>
2023-11-09 19:18:01 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ from sklearn.metrics import mean_squared_error, r2_score
 import matplotlib.pyplot as plt
 import numpy as np
 
-# 가상 데이터 생성
+# 가상 데이터 생성 (임의로 생성)
+# 예시 데이터 (날짜, 강우량, 기온, 구름의 양, 수온)
 data = {
     '날짜': pd.date_range('20230101', periods=100),
     'rainfall': [i % 10 for i in range(100)],
@@ -18,6 +19,12 @@ data = {
 # 데이터프레임 생성
 df = pd.DataFrame(data)
 
+st.title('Linear Regression Model')
+
+# 사이드바에서 학습 및 테스트 섅트 비율 선택
+test_sizes = [i / 10 for i in range(1, 10)]
+test_size = st.sidebar.selectbox('Select Test Set Size', options=test_sizes)
+
 # Features(특징) 및 Target(목표) 데이터 설정
 features = ['rainfall', '기온', '구름의양']
 target = '수온'
@@ -26,7 +33,7 @@ X = df[features]
 y = df[target]
 
 # 학습 및 테스트 데이터 분할
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=42)
 
 # 선형 회귀 모델 초기화 및 학습
 model = LinearRegression()
@@ -37,15 +44,10 @@ predictions = model.predict(X_test)
 
 # 모델 성능 측정
 mse = mean_squared_error(y_test, predictions)
-r2 = r2_score(y_test, predictions)
+st.write(f"Mean Squared Error: {mse}")
 
-# Streamlit 앱 구성
-st.title('Linear Regression Model Results')
-st.write(f"Mean Squared Error: {mse:.2f}")
-st.write(f"R2 Score: {r2:.2f}")
-
-# 테스트 데이터에서의 실제 값과 모델 예측 값 비교 그래프
-fig, ax = plt.subplots(figsize=(10, 5))
+# 그래프 생성
+fig, ax = plt.subplots()
 ax.scatter(y_test, predictions, label='compare', color='orange')
 ax.set_xlabel('test measured')
 ax.set_ylabel('model predicted')
@@ -55,5 +57,9 @@ ax.set_title('measured vs predicted')
 z = np.polyfit(y_test, predictions, 1)
 p = np.poly1d(z)
 ax.plot(y_test, p(y_test), color='red')
+
+# R2 값 표시
+r2 = r2_score(y_test, predictions)
+ax.text(0.1, 0.9, f'R2 = {r2:.2f}', ha='center', va='center', transform=ax.transAxes, fontsize=10)
 
 st.pyplot(fig)
